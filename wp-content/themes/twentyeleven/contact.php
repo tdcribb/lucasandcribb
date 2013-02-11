2b974070f4f3c90874ef049408e3cf2b7c0c2e21
Tod Cribb <tdcribb@gmail.com>
2013-02-11 10:15:05 -0500

    update to css and contact page

--- wp-content/themes/twentyeleven/contact.php
+++ wp-content/themes/twentyeleven/contact.php
@@ -18,7 +18,7 @@ Template Name: Contact
 		</div><!-- #content -->
 
 		<div id="map-cont">
-			<iframe width="450" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=charleston+sc&amp;sll=33.624497,-80.926614&amp;sspn=6.867654,9.63501&amp;ie=UTF8&amp;hq=&amp;hnear=Charleston,+South+Carolina&amp;ll=32.776566,-79.930922&amp;spn=0.054191,0.075274&amp;t=m&amp;z=14&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=charleston+sc&amp;sll=33.624497,-80.926614&amp;sspn=6.867654,9.63501&amp;ie=UTF8&amp;hq=&amp;hnear=Charleston,+South+Carolina&amp;ll=32.776566,-79.930922&amp;spn=0.054191,0.075274&amp;t=m&amp;z=14" style="color:#ffffff;text-align:left">View Larger Map</a></small>
+			<iframe width="450" height="550" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=charleston+sc&amp;sll=33.624497,-80.926614&amp;sspn=6.867654,9.63501&amp;ie=UTF8&amp;hq=&amp;hnear=Charleston,+South+Carolina&amp;ll=32.776566,-79.930922&amp;spn=0.054191,0.075274&amp;t=m&amp;z=14&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=charleston+sc&amp;sll=33.624497,-80.926614&amp;sspn=6.867654,9.63501&amp;ie=UTF8&amp;hq=&amp;hnear=Charleston,+South+Carolina&amp;ll=32.776566,-79.930922&amp;spn=0.054191,0.075274&amp;t=m&amp;z=14" style="color:#ffffff;text-align:left">View Larger Map</a></small>
 		</div>
 
 
